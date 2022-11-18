dddee9a7af6b604b14457f4a182030111ba09297
SaracensFan <matthewclifford@hotmail.co.uk>
2022-11-18 16:11:17 +0000

    update to port

--- app.js
+++ app.js
@@ -9,7 +9,19 @@ mongoose.connect(process.env.MONGO_DB)
 
 const blogRouter = require('./routes/blogRouter')
 
-let port = 3000
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 app.get('/', blogRouter)
 app.get('/api/blog', blogRouter)
